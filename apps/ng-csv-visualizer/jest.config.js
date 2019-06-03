01489c393a2b0358b3614f332534a1097a1573d8
Ramon Osenga <Ramon.Osenga@Frontmen.nl>
2019-06-03 04:02:24 +0200

    Auto-generated commit

--- apps/ng-csv-visualizer/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'ng-csv-visualizer',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/ng-csv-visualizer',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
--- libs/material/README.md
+++
@@ -1,7 +0,0 @@
-# material
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `ng test material` to execute the unit tests.
